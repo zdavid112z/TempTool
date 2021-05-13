3cbbc37e9ce48f5ad7f78a13117842661bf291a8
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-05-13 05:01:45 -0400

    add requirement

--- Server/run_backend.sh
+++ Server/run_backend.sh
@@ -5,8 +5,13 @@ cd /www/backend
 while true; do
     # Run server
     # python3 -m swagger_server
+<<<<<<< HEAD
     python3 ServerFiles/server.py
+<<<<<<< Updated upstream
     sleep 3
+=======
+=======
+    python3 ServerFile/server.py
     echo "Backend crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
